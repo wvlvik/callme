d39c4760b09e4c992bf45db76ec3f0366666c49b
lv <lv@lvblank.local>
2018-06-20 15:02:53 +0800

    add

--- src/logic/api/apply.js
+++ src/logic/api/apply.js
@@ -6,9 +6,17 @@ module.exports = class extends think.Logic {
   async postAction() {
 
   	let post = this.post();
-  	let errorCode = '内容为空';
+  	let errorCode = '内容未填写完整';
 
-  	if(think.isEmpty(post.name) || think.isEmpty(post.tel)) {
+  	if(post.type == 2) {
+  		text = post.custom_word;
+  	}else if(post.type == 3) {
+  		text = post.custom_text;
+  	}else {
+  		text = post.name;
+  	}
+
+  	if(think.isEmpty(text) || think.isEmpty(post.tel)) {
   		return this.fail(errorCode);
   	}
 
